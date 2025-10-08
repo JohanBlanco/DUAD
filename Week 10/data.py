8fb9a3546eb04a20d377b5d48202d334dd5a004e
JohanBlanco <blajohanst@gmail.com>
2025-10-08 10:16:06 -0600

    update

--- Week 10/data.py
+++ Week 10/data.py
@@ -1,1 +1,16 @@
 # Contans the logic for importing and exporting the data
+
+import csv
+
+
+def read_csv_file(file_path):
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            print(row)
+
+def write_csv_file(file_path, data, headers):
+    with open(file_path, 'w', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, headers)
+        writer.writeheader()
+        writer.writerows(data)
