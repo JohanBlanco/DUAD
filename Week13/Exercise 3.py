e7069eba3ca0db40bb961e5b0aa15f16cfe4dd4c
JohanBlanco <blajohanst@gmail.com>
2025-10-18 00:33:34 -0600

    tested it

--- Week13/Exercise 3.py
+++ Week13/Exercise 3.py
@@ -1,14 +1,14 @@
-import datetime
+from datetime import datetime
 
 class User:
 
     def __init__(self, date_of_birth):
-        self.date_of_birth = datetime.strptime(date_of_birth, "%d-%m-%Y").date()
+        self.date_of_birth = datetime.strptime(date_of_birth, "%d/%m/%Y").date()
 
     @property
-    def age(cls, self):
-        current_year = datetime.date.today().year
-        current_month = datetime.date.today().month
+    def age(self):
+        current_year = datetime.today().year
+        current_month = datetime.today().month
 
         birth_year = self.date_of_birth.year
         birth_moth = self.date_of_birth.month
@@ -19,10 +19,24 @@ class User:
             age -= 1
 
         return age
-    
-def decorator(func):
-        def wrapper(*parameters):
-            print(f"Parameters from Wrapper before the function: {parameters}")
-            func(parameters)
-            print(f"Parameters from Wrapper after the function: {parameters}")
+
+def only_adults(func):
+        def wrapper(user:User):
+            age = user.age
+            if age < 18:
+                raise ValueError(f'The user must be an adult, and you are {age} y.o')
+            func(User)
         return wrapper
+
+@only_adults
+def buy_drugs(user:User):
+    print('Your order number is AQWE12321')
+
+
+if __name__ == '__main__':
+    buy_drugs(User('12/10/1999'))
+
+    try:
+        buy_drugs(User('12/10/2009'))
+    except ValueError as e:
+        print(e)
