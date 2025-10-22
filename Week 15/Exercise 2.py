a5f0ac1d8efb269f446b4b8f96cb2a63f756ec2c
JohanBlanco <blajohanst@gmail.com>
2025-10-22 11:35:00 -0600

    renamed the function

--- Week 15/Exercise 2.py
+++ Week 15/Exercise 2.py
@@ -1,4 +1,4 @@
-def inverted_bubble_sort(list_to_sort):
+def reversed_bubble_sort(list_to_sort):
     for _ in range(len(list_to_sort) - 1, -1, -1):
         changed = False
         for index in range(len(list_to_sort) - 1, 0, -1):
@@ -11,13 +11,12 @@ def inverted_bubble_sort(list_to_sort):
                 changed = True
         if not changed:
             break
-    return list_to_sort
 
 if __name__ == "__main__":
     list1 = [5, 2, 9, 1, 5, 6]
 
     print(list1)
 
-    inverted_bubble_sort(list1)
+    reversed_bubble_sort(list1)
 
     print(list1)
