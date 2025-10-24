72b0900bdf2b2e19074d9e90a845175394edf2b9
JohanBlanco <blajohanst@gmail.com>
2025-10-24 15:03:54 -0600

    :)

--- week_16/tests/test_week6_exercise_2.py
+++ week_16/tests/test_week6_exercise_2.py
@@ -1,2 +1,21 @@
 import pytest
 from week_6.exercise_5_task_functions import revert_string
+from week_6.exercise_8_task_functions import get_prime_numbers
+
+def test_revert_string_works_well():
+    # Arrange
+    string = "Hola Mundo"
+    
+    # Act
+    result = revert_string(string) 
+    
+    # Assert
+    assert result == string[::-1]
+
+def test_get_prime_numbers_works_well():
+    # Arrange
+
+    # Act
+
+    # Assert
+    pass
