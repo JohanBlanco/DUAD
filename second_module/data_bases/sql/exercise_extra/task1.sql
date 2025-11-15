1efc303163a42af1adb5bdda7f340c136bb1b97e
JohanBlanco <blajohanst@gmail.com>
2025-11-15 11:47:24 -0600

    update

--- second_module/data_bases/sql/exercise_extra/task1.sql
+++ second_module/data_bases/sql/exercise_extra/task1.sql
@@ -1,7 +1,3 @@
-DROP TABLE IF EXISTS products;
-DROP TABLE IF EXISTS categories;
-DROP TABLE IF EXISTS invoices;
-DROP TABLE IF EXISTS sales;
 -- Cree la tabla categories con: id (PK autoincrement), name (UNIQUE, NOT NULL), description
 CREATE TABLE categories(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -10,35 +6,20 @@ CREATE TABLE categories(
 );
 
 -- Agregue a products la columna category_id (INTEGER, puede permitir NULL)
-CREATE TABLE products(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(50) NOT NULL,
-    code VARCHAR(100) UNIQUE NOT NULL,
-    price INTEGER NOT NULL,
-    date_received DATE NOT NULL,
-    brand VARCHAR(100) NOT NULL,
-    category_id INTEGER,
+ALTER TABLE products 
+ADD COLUMN category_id INTEGER REFERENCES categories(id);
 
-    FOREIGN KEY (category_id) REFERENCES categories(id)
-);
-
--- Inserte al menos 3 filas en categories
+-- -- Inserte al menos 3 filas en categories
 INSERT INTO categories(name, description) VALUES('Fast Food', 'This category is for junk food');
 INSERT INTO categories(name, description) VALUES('Healthy Food', 'This category is for healthy food');
 INSERT INTO categories(name, description) VALUES('Beverage', 'This category is for any beverage');
 
-
--- Actualice algunos products asignándoles un category_id
-INSERT INTO products(name, code, price,date_received, brand, category_id) VALUES('H20', '001', 100000, '2025-11-09', 'Dos Pinos', NULL);
-INSERT INTO products(name, code, price,date_received, brand, category_id) VALUES('Coca Cola', '002', 200000, '2025-11-09', 'Coca Cola', NULL);
-INSERT INTO products(name, code, price,date_received, brand, category_id) VALUES('Pizza', '003', 30000, '2025-11-09', 'Dominos', NULL);
-INSERT INTO products(name, code, price,date_received, brand, category_id) VALUES('Hamburguer', '004', 40000, '2025-11-09', 'Burguer king', NULL);
-
+-- -- Actualice algunos products asignándoles un category_id
 UPDATE products SET category_id = 3 WHERE id = 1;
 UPDATE products SET category_id = 3 WHERE id = 2;
 UPDATE products SET category_id = 1 WHERE id = 3;
 UPDATE products SET category_id = 1 WHERE id = 4;
 
--- Verifique con SELECT * FROM products (muestre id, product_name, price, quantity, category_id)
+-- -- Verifique con SELECT * FROM products (muestre id, product_name, price, quantity, category_id)
 SELECT * FROM products;
 
--- second_module/data_bases/sql/exercise_extra/task4.sql
+++ second_module/data_bases/sql/exercise_extra/task4.sql
@@ -1,23 +1,8 @@
-CREATE TABLE sales(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    product_id INTEGER NOT NULL,
-    invoice_id INTEGER NOT NULL,
-    quantity INTEGER NOT NULL,
-    total_price INTEGER NOT NULL,
-
-    FOREIGN KEY (product_id) REFERENCES products(id),
-    FOREIGN KEY (invoice_id) REFERENCES invoices(id)
-);
-
-INSERT INTO sales(product_id, invoice_id, quantity, total_price) VALUES(1, 1, 2, 200000);
-INSERT INTO sales(product_id, invoice_id, quantity, total_price) VALUES(2, 1, 2, 400000);
-INSERT INTO sales(product_id, invoice_id, quantity, total_price) VALUES(3, 1, 2, 60000);
-INSERT INTO sales(product_id, invoice_id, quantity, total_price) VALUES(4, 1, 2, 80000);
-
-INSERT INTO sales(product_id, invoice_id, quantity, total_price) VALUES(1, 2, 1, 100000);
-INSERT INTO sales(product_id, invoice_id, quantity, total_price) VALUES(3, 2, 1, 30000);
-
 -- Establezca quantity = 0 donde price <= 0
+-- select p.id, i.quantity, p.price
+-- from shopping_cart_items i
+-- join products p
+-- where p.id = i.product_id;
 
 
 -- Aumente el price en 100 unidades para todos los productos cuando quantity sea menor a 10
