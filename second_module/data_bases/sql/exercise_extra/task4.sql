49488173687f92fa3674c838ddd7c6b0b92aa417
JohanBlanco <blajohanst@gmail.com>
2025-11-15 12:40:52 -0600

    sql commit

--- second_module/data_bases/sql/exercise_extra/task4.sql
+++ second_module/data_bases/sql/exercise_extra/task4.sql
@@ -1,15 +1,30 @@
 -- Establezca quantity = 0 donde price <= 0
--- select p.id, i.quantity, p.price
--- from shopping_cart_items i
--- join products p
--- where p.id = i.product_id;
-
+select  prod.id product_id, prod.price price, 
+        CASE WHEN prod.price <= 0 
+                THEN 0
+            ELSE
+                item.quantity
+        END AS quantity
+from shopping_cart_items item
+join products prod
+where item.product_id = prod.id;
 
 -- Aumente el price en 100 unidades para todos los productos cuando quantity sea menor a 10
-
+select  prod.id product_id, prod.price price, item.quantity,
+        CASE WHEN item.quantity <= 10
+                THEN prod.price + 100
+            ELSE
+                prod.price
+        END AS increassed_price
+from shopping_cart_items item
+join products prod
+where item.product_id = prod.id;
 
 -- Disminuya quantity en 1 para un product_id específico
-
+select  prod.id product_id, item.quantity, (item.quantity - 1) decreassed_by_1_quantity
+from shopping_cart_items item
+join products prod
+where item.product_id = prod.id and prod.id = 1;
 
 -- Verifique con SELECT * FROM products ORDER BY id ASC LIMIT 10
-
+SELECT * FROM products ORDER BY id ASC LIMIT 10;
