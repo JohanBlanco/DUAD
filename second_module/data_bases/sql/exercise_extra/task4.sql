37f78f5a45816e3d821f3b5fd3ffef47b1e56309
JohanBlanco <blajohanst@gmail.com>
2025-11-19 13:19:12 -0600

    update

--- second_module/data_bases/sql/exercise_extra/task4.sql
+++ second_module/data_bases/sql/exercise_extra/task4.sql
@@ -1,30 +1,25 @@
 -- Establezca quantity = 0 donde price <= 0
-select  prod.id product_id, prod.price price, 
-        CASE WHEN prod.price <= 0 
-                THEN 0
-            ELSE
-                item.quantity
-        END AS quantity
-from shopping_cart_items item
-join products prod
-where item.product_id = prod.id;
+UPDATE shopping_cart_items
+SET quantity = 0
+WHERE product_id IN (
+    SELECT id
+    FROM products
+    WHERE price <= 0
+);
 
 -- Aumente el price en 100 unidades para todos los productos cuando quantity sea menor a 10
-select  prod.id product_id, prod.price price, item.quantity,
-        CASE WHEN item.quantity <= 10
-                THEN prod.price + 100
-            ELSE
-                prod.price
-        END AS increassed_price
-from shopping_cart_items item
-join products prod
-where item.product_id = prod.id;
+UPDATE products
+SET price = price + 100
+WHERE id IN (
+    SELECT product_id
+    FROM shopping_cart_items
+    WHERE quantity <= 10
+);
 
 -- Disminuya quantity en 1 para un product_id específico
-select  prod.id product_id, item.quantity, (item.quantity - 1) decreassed_by_1_quantity
-from shopping_cart_items item
-join products prod
-where item.product_id = prod.id and prod.id = 1;
+UPDATE shopping_cart_items
+SET quantity = quantity - 1
+WHERE product_id = 1;
 
--- Verifique con SELECT * FROM products ORDER BY id ASC LIMIT 10
+-- -- Verifique con SELECT * FROM products ORDER BY id ASC LIMIT 10
 SELECT * FROM products ORDER BY id ASC LIMIT 10;
